3e7a4accda09fdce325ec55b83f95ed3ef8ec200
BrieannaMann <brieanna.mann1@gmail.com>
2017-07-25 11:58:18 -0400

    lunch backup
    finished project

--- comp/js/newyorktimes.js
+++ comp/js/newyorktimes.js
@@ -1,5 +1,6 @@
 
 $("#selectMenu").on("change", function(){
+
 	$('#title').empty();
 	var val= $("#selectMenu option:selected").val()
 	var url = "https://api.nytimes.com/svc/topstories/v2/" + val +".json";
@@ -11,7 +12,7 @@ $.ajax({
  url: url,
  method: 'GET',
 }).done(function(result) {
-	$("header").addclass("minified");
+
 var count=1;
 	for(var i= 0; i< result.results.length; i++){
 			
@@ -21,8 +22,10 @@ var count=1;
 			var photourl = '<img src=" '+ result.results[i].multimedia[4].url +'  "/>';
 			var abstract = '<p  class="abstract">'+result.results[i].abstract +'</p>';
 			var articlelink = result.results[i].url;
+
 					$(".images").append(( '<div class="article-mobile">' + photourl + '<a href="' + articlelink +'">' + abstract +'</a></div>'));
 					$(count++);
+					$('.header').removeClass('header').addClass('minified');
 				}}
 
 				else {
